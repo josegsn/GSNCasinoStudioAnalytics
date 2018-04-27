821dcd61750c1911dd4257abf85a15c54286d04b
josegsn <jnaranjo@gsngames.com>
2018-04-27 11:24:19 +0100

    New Segments to sync

---
+++ Swrve Segment Queries/Swrve_TESTING_SEGMENT_Daily.sql
@@ -0,0 +1,29 @@
+WITH segments AS (
+SELECT 
+	attr3 AS mesmo_id,
+	CASE
+		WHEN REGEXP_SUBSTR(attr3, '\d\d$') BETWEEN 00 AND 19 THEN 'A'
+		WHEN REGEXP_SUBSTR(attr3, '\d\d$') BETWEEN 20 AND 39 THEN 'B'
+		WHEN REGEXP_SUBSTR(attr3, '\d\d$') BETWEEN 40 AND 59 THEN 'C'
+		WHEN REGEXP_SUBSTR(attr3, '\d\d$') BETWEEN 60 AND 79 THEN 'D'
+		WHEN REGEXP_SUBSTR(attr3, '\d\d$') BETWEEN 80 AND 99 THEN 'E'
+		END AS segment
+
+FROM gsnmobile.events
+WHERE app_name = 'GSN Casino'
+	AND event_name = 'checkpoint'	
+	AND event_day BETWEEN date('{{ ds }}') - 30 AND date('{{ ds }}')
+	AND attr3 IS NOT NULL
+GROUP BY 1,2
+HAVING MIN(event_day) = date('{{ ds }}')
+)
+
+
+SELECT
+	DISTINCT
+	swrve_user_id as "user",
+	segment as TESTING_SEGMENT
+FROM segments a
+
+JOIN gsnmobile.swrve_casino_mapping y
+	ON a.mesmo_id = y.mesmoid
